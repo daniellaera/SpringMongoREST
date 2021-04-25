ccbce6232220478802fc752710a319fa1ec1cb08
daniellaera <laera.daniel@hotmail.com>
2021-04-25 12:30:53 +0200

    hotfix: readme & controller crud methods

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 This is a Spring Boot project bootstrapped with Spring Initializr https://start.spring.io/
+
+### Installation Steps:
+
+```
+> git clone https://github.com/daniellaera/SpringMongoREST
+> Load maven dependencies by Right clicking on Project -> Maven -> Reload project
+> Set your mongo connection uri in application.properties
+> Run springboot application
+```
+
+By default the application runs on localhost:8080
--- src/main/java/com/example/SpringMongoREST/controller/MovieController.java
+++ src/main/java/com/example/SpringMongoREST/controller/MovieController.java
@@ -6,10 +6,7 @@ import com.example.SpringMongoREST.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolationException;
 import java.util.List;
@@ -21,22 +18,51 @@ public class MovieController {
     private MovieService movieService;
 
     @RequestMapping(method = RequestMethod.POST, value = "/movie")
-    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+    public ResponseEntity<String> createMovie(@RequestBody Movie movie) {
         try {
             movieService.createMovie(movie);
-            return new ResponseEntity("Successfully added movie" + movie.getTitle(), HttpStatus.OK);
-        }
-        catch (ConstraintViolationException e) {
+            return new ResponseEntity("Successfully added movie " + movie.getTitle(), HttpStatus.OK);
+        } catch (ConstraintViolationException e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
-        }
-        catch (MovieCollectionException e) {
+        } catch (MovieCollectionException e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.CONFLICT);
         }
     }
 
+
     @RequestMapping(method = RequestMethod.GET, value = "/movie")
     public ResponseEntity getAllMovies() {
         List<Movie> movies = movieService.getAllMovies();
-        return new ResponseEntity(movies, movies.size() > 0 ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+        return new ResponseEntity(
+                movies,
+                movies.size() > 0 ? HttpStatus.OK : HttpStatus.NOT_FOUND
+        );
+
     }
+
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/movie/{id}")
+    public ResponseEntity deleteMovieById(@PathVariable("id") String id) {
+        try {
+            movieService.deleteMovieById(id);
+            return new ResponseEntity("Successfully deleted movie with id " + id, HttpStatus.OK);
+        } catch (MovieCollectionException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/movie/{id}")
+    public ResponseEntity updateById(@PathVariable("id") String id, @RequestBody Movie newMovie) {
+        try {
+            movieService.updateMovie(id, newMovie);
+            return new ResponseEntity("Updated movie with id " + id + "", HttpStatus.OK);
+        } catch (ConstraintViolationException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+        } catch (MovieCollectionException e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+
+    }
+
 }
